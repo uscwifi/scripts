9f9c6bbd1434948ba639e6676142f1d10cbb8659
uscwifi <42139780+uscwifi@users.noreply.github.com>
2019-01-14 18:41:03 +0800

    Update check_crt.sh

--- check_crt.sh
+++ check_crt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #host="/root/.acme.sh/cagetest.com/fullchain.cer"
-crt_path="/etc/haproxy/ssl/onemicroworld.com_2019.pem"
+crt_path="/etc/haproxy/ssl/xxx.com_2019.pem"
 end_date=`openssl x509 -in ${crt_path} -noout -text | grep "Not After" | awk  -F'Not After :' '{print $2 }'`
 now_date_second=`date +%s`
 end_date_second=`date +%s --date "${end_date}"`
