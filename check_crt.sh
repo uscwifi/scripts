e2a9c68105554171f302fa4631ca1d14411cb621
uscwifi <42139780+uscwifi@users.noreply.github.com>
2019-01-14 18:40:30 +0800

    zabbix检查证书文件到期时间
    咱不必须检查证书文件到期时间

---
+++ check_crt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#host="/root/.acme.sh/cagetest.com/fullchain.cer"
+crt_path="/etc/haproxy/ssl/onemicroworld.com_2019.pem"
+end_date=`openssl x509 -in ${crt_path} -noout -text | grep "Not After" | awk  -F'Not After :' '{print $2 }'`
+now_date_second=`date +%s`
+end_date_second=`date +%s --date "${end_date}"`
+echo  "(${end_date_second}-${now_date_second})/3600/24" | bc
+
