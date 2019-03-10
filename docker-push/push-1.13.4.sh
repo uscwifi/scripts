1c958638dd440e8ab0cc688df0478e91daaedc25
root <root@localhost.localdomain>
2019-03-10 20:07:38 +0000

    add flannel image.

--- docker-push/push-1.13.4.sh
+++ docker-push/push-1.13.4.sh
@@ -1,11 +1,15 @@
 registry="k8s.gcr.io"
 path="registry.cn-shanghai.aliyuncs.com/kdc/"
 kube_tag="v1.13.4"
-images="kube-proxy:${kube_tag} kube-apiserver:${kube_tag} kube-controller-manager:${kube_tag} kube-scheduler:${kube_tag} coredns:1.2.6 etcd:3.2.24 pause:3.1"
+images="kube-proxy:${kube_tag} kube-apiserver:${kube_tag} kube-controller-manager:${kube_tag} kube-scheduler:${kube_tag} coredns:1.2.6 etcd:3.2.24 pause:3.1 flannel:v0.11.0-amd64"
 
 for i in ${images}
 do
+    if [ ${i} = 'flannel:v0.11.0-amd64' ];then
+	registry="quay.io/coreos"
+    fi
     docker tag ${registry}/${i} ${path}${i}
     docker push ${path}${i}
     docker image rm ${path}${i}
+    registry="k8s.gcr.io"
 done
