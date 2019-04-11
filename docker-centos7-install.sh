613a95e831935c83734a22cec2b3bdee66695b52
uscwifi <42139780+uscwifi@users.noreply.github.com>
2019-04-11 13:08:01 +0800

    Update docker-centos7-install.sh

--- docker-centos7-install.sh
+++ docker-centos7-install.sh
@@ -18,3 +18,12 @@ EOF
 systemctl daemon-reload 
 systemctl restart docker
 systemctl enable docker
+
+#核心路由转发
+modprobe br_netfilter
+cat >> /etc/sysctl.d/docker.conf <<EOF
+net.ipv4.ip_forward = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl -p /etc/sysctl.d/docker.conf
