f0b93bd89f9eeecf30ac67d250ba5b225a3ac573
admin <admin@uscwifi.cn>
2019-05-07 16:55:42 +0800

    add pull-1.14.1.sh

---
+++ docker-push/push-1.14.1.sh
@@ -0,0 +1,15 @@
+registry="k8s.gcr.io"
+path="registry.cn-shanghai.aliyuncs.com/kdc/"
+kube_tag="v1.14.1"
+images="kube-proxy:${kube_tag} kube-apiserver:${kube_tag} kube-controller-manager:${kube_tag} kube-scheduler:${kube_tag} coredns:1.3.1 etcd:3.3.10 pause:3.1 flannel:v0.11.0-amd64"
+
+for i in ${images}
+do
+    if [ ${i} = 'flannel:v0.11.0-amd64' ];then
+	registry="quay.io/coreos"
+    fi
+    docker tag ${registry}/${i} ${path}${i}
+    docker push ${path}${i}
+    docker image rm ${path}${i}
+    registry="k8s.gcr.io"
+done
